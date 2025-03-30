d0702fd1a951c3b6d6196aef900aac211745bdc1
Lucas Vinicius de Oliva <78659818+2312-lucj@users.noreply.github.com>
2025-03-30 17:37:00 +0000

    adiciona scripts `lint:check` e `lint:fix`

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,11 @@
 function Home() {
-    return <h1>Bom dia, boa tarde e boa noite, minha irmã é so um teste aqui </h1>
+  return (
+    <h1>Bom dia, boa tarde e boa noite, minha irmã é so um teste aqui </h1>
+  );
+}
+
+function teste() {
+  console.log("indentacao errada");
 }
 
 export default Home;
